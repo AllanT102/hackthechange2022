9c1deda82db700333e2cd267d1ea4fd72697a9dc
Allan Tan <allan@dhcp-128-189-79-158.ubcsecure.wireless.ubc.ca>
2022-11-19 10:57:43 -0800

    init

---
+++ server/index.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json()); // specifies that all incoming data is json format
