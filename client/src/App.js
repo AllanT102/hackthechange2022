5a58406df7814c8b948b1b6058c84d2344f30c31
Allan Tan <allan@dhcp-128-189-79-158.ubcsecure.wireless.ubc.ca>
2022-11-19 15:10:17 -0800

    connected fe and be

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
 import StartPanel from "./components/startPanel";
+import axios from "axios"
+import { useEffect, useState } from "react";
 
 function App() {
+  const [text, setText] = useState();
+  useEffect(() => {
+    axios.get("http://localhost:3001/getQuizData")
+    .then((res) =>{
+      setText(res.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }, [])
+
   return (
     <div>
       <StartPanel />
--- server/index.js
+++ server/index.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const router = express.Router();
 app.use(cors());
 app.use(express.json()); // specifies that all incoming data is json format
+
 const http = require("https");
 
-app.get("/getQuizData", (req, res) => {   
-    const result = obtainQuizData();
-    res.send(result);
-})
 
-const obtainQuizData = () => {
+app.get("/getQuizData", async (request, response) => {
     const url = "https://quizlet.com/ca/639971093/dsci100-quiz-2-flash-cards/"
     const options = {
         "method": "GET",
@@ -19,23 +17,23 @@ const obtainQuizData = () => {
         "path":  `/v1?url=${url}%2F&api_key=A2fwN9zTxMhBXbwa2BSaEQbazFjcPHbq&render_js=0&device=desktop&proxy_type=datacenter&extract_rules=%7B%22selector%22%3A%22.TermText%22%2C%20%22output%22%3A%22text%22%7D`,
         "headers": {}
     };
-
+    
     const req = http.request(options, function (res) {
     const chunks = [];
-
+    let result = "";
+    
     res.on("data", function (chunk) {
         chunks.push(chunk);
     });
-
+    
     res.on("end", function () {
         const body = Buffer.concat(chunks);
-        console.log(body.toString());
+        response.send(body.toString());
     });
     });
-
+    
     req.end();
-    return body.toString();
-}
+})
 
 app.listen(3001, () => {
     console.log("server running on port 3001");
