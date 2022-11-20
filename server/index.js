e63d78d85bead2b183f6277f70f7b33e655b0d3e
Allan Tan <allan@dhcp-128-189-79-158.ubcsecure.wireless.ubc.ca>
2022-11-19 16:00:36 -0800

    working on monkey learn api route

--- server/index.js
+++ server/index.js
@@ -1,39 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const router = express.Router();
 app.use(cors());
 app.use(express.json()); // specifies that all incoming data is json format
 
-const http = require("https");
+// routers
+const quizRouter = require('./routes/QuizData');
+app.use("/getQuizData", quizRouter);
+const keywordRouter = require('./routes/Keywords');
+app.use("/getKeywords", keywordRouter);
 
-
-app.get("/getQuizData", async (request, response) => {
-    const url = "https://quizlet.com/ca/639971093/dsci100-quiz-2-flash-cards/"
-    const options = {
-        "method": "GET",
-        "hostname": "api.webscrapingapi.com",
-        "port": null,
-        "path":  `/v1?url=${url}%2F&api_key=A2fwN9zTxMhBXbwa2BSaEQbazFjcPHbq&render_js=0&device=desktop&proxy_type=datacenter&extract_rules=%7B%22selector%22%3A%22.TermText%22%2C%20%22output%22%3A%22text%22%7D`,
-        "headers": {}
-    };
-    
-    const req = http.request(options, function (res) {
-    const chunks = [];
-    let result = "";
-    
-    res.on("data", function (chunk) {
-        chunks.push(chunk);
-    });
-    
-    res.on("end", function () {
-        const body = Buffer.concat(chunks);
-        response.send(body.toString());
-    });
-    });
-    
-    req.end();
-})
 
 app.listen(3001, () => {
     console.log("server running on port 3001");
---
+++ server/routes/Keywords.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+    const MonkeyLearn = require('monkeylearn')
+
+    const ml = new MonkeyLearn('484b02e0fc16ee4eec78289b9ec2f8b97792bafa')
+    let model_id = 'ex_MKMcW3w9'
+    let data = ["The food was delicious and service was great!"]
+    await ml.extractors.extract(model_id, data)
+    .then(response => {
+        res.send(response.body);
+    })
+})
+
+module.exports = router;
